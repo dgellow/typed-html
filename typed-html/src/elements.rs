a29b7a2f4d25c81f5b83808d8ea6528f3e3396f0
Samuel El-Borai <sam@elborai.me>
2022-01-26 18:05:29 +0100

    Support additional SVG related attributes

--- typed-html/src/elements.rs
+++ typed-html/src/elements.rs
@@ -292,6 +292,9 @@ declare_elements! {
     path { // TODO: support all SVG presentation attributes
         d: String,
         fill: String, // TODO: spec defines value as "none | <color> | <url> [none | <color>]? | context-fill | context-stroke"
+        stroke-linecap: String, // TODO: too permissive and should be generalized to other elements as well
+        stroke-width: String, // TODO: too permissive and should be generalized to other elements as well
+        stroke-linejoin: String, // TODO: too permissive and should be generalized to other elements as well
     } in [SvgContent];
     pre in [FlowContent] with PhrasingContent;
     progress {
@@ -343,6 +346,8 @@ declare_elements! {
         y: String, // TODO: spec defines value as "<length> | <percentage>"
         width: String, // TODO: spec defines value as "<length> | <percentage> | auto | inherit"
         height: String, // TODO: spec defines value as "<length> | <percentage> | auto | inherit"
+        fill: String, // TODO: too permissive and should be generalized to other elements as well
+        stroke: String, // TODO: too permissive and should be generalized to other elements as well
     } in [FlowContent, PhrasingContent, EmbeddedContent] with SvgContent; // TODO: should support other SVG nodes
     table in [FlowContent] with TableContent;
     template in [MetadataContent, FlowContent, PhrasingContent, TableColumnContent] with Node;
@@ -504,6 +509,23 @@ fn test_svg_element_1() {
 
     assert_eq!(
         r#"<svg class="fa-rss fa-w-14 svg-inline--fa" id="fas_fa-rss" role="img" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg" data-icon="rss" data-prefix="fas"><path d="M128.081 415.959c0 35.369-28.672 64.041-64.041 64.041S0 451.328 0 415.959s28.672-64.041 64.041-64.041 64.04 28.673 64.04 64.041zm175.66 47.25c-8.354-154.6-132.185-278.587-286.95-286.95C7.656 175.765 0 183.105 0 192.253v48.069c0 8.415 6.49 15.472 14.887 16.018 111.832 7.284 201.473 96.702 208.772 208.772.547 8.397 7.604 14.887 16.018 14.887h48.069c9.149.001 16.489-7.655 15.995-16.79zm144.249.288C439.596 229.677 251.465 40.445 16.503 32.01 7.473 31.686 0 38.981 0 48.016v48.068c0 8.625 6.835 15.645 15.453 15.999 191.179 7.839 344.627 161.316 352.465 352.465.353 8.618 7.373 15.453 15.999 15.453h48.068c9.034-.001 16.329-7.474 16.005-16.504z" fill="currentColor"></path></svg>"#,
+        elem.to_string()
+    );
+}
+
+#[test]
+fn test_svg_element_2() {
+    use crate as typed_html;
+    use crate::{dom::DOMTree, html};
+
+    let elem: DOMTree<String> = html! {
+        <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+    };
+
+    assert_eq!(
+        r#"<svg class="h-6 hidden w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path d="M6 18L18 6M6 6l12 12" stroke_linecap="round" stroke_linejoin="round" stroke_width="2"></path></svg>"#,
         elem.to_string()
     );
 }
